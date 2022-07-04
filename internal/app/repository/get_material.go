87c11f47f90320b5ea60a7c3c47277b018c4aa7c
ivan <isdemchuk@gmail.com>
2022-07-04 20:29:22 +0300

    getValues, getProducts added

---
+++ internal/app/repository/get_material.go
@@ -0,0 +1,37 @@
+package repository
+
+import (
+	"context"
+	"fmt"
+	"metallplace/internal/pkg/db"
+)
+
+func (r *Repository) GetMaterialId(ctx context.Context, materialName string) (int, error) {
+	var id int
+	row, err := db.FromContext(ctx).QueryRow(`SELECT id FROM material WHERE name=$1`, materialName)
+	if err != nil {
+		return 0, fmt.Errorf("Can't get material id %w", err)
+	}
+
+	err = row.Scan(&id)
+	if err != nil {
+		return 0, fmt.Errorf("Can't get id with row.Scan() %w", err)
+	}
+
+	return id, nil
+}
+
+func (r *Repository) GetMaterialName(ctx context.Context, materialId int) (string, error) {
+	var name string
+	row, err := db.FromContext(ctx).QueryRow(`SELECT name FROM material WHERE id=$1`, materialId)
+	if err != nil {
+		return "", fmt.Errorf("Can't get material name %w", err)
+	}
+
+	err = row.Scan(&name)
+	if err != nil {
+		return "", fmt.Errorf("Can't get material name with row.Scan() %w", err)
+	}
+
+	return name, nil
+}
---
+++ internal/app/repository/get_source.go
@@ -0,0 +1,37 @@
+package repository
+
+import (
+	"context"
+	"fmt"
+	"metallplace/internal/pkg/db"
+)
+
+func (r *Repository) GetSourceId(ctx context.Context, sourceName string) (int, error) {
+	var id int
+	row, err := db.FromContext(ctx).QueryRow(`SELECT id FROM source WHERE name=$1`, sourceName)
+	if err != nil {
+		return 0, fmt.Errorf("Can't get source id %w", err)
+	}
+
+	err = row.Scan(&id)
+	if err != nil {
+		return 0, fmt.Errorf("Can't get source id with row.Scan() %w", err)
+	}
+
+	return id, nil
+}
+
+func (r *Repository) GetSourceName(ctx context.Context, sourceId int) (string, error) {
+	var name string
+	row, err := db.FromContext(ctx).QueryRow(`SELECT name FROM source WHERE id=$1`, sourceId)
+	if err != nil {
+		return "", fmt.Errorf("Can't get source name %w", err)
+	}
+
+	err = row.Scan(&name)
+	if err != nil {
+		return "", fmt.Errorf("Can't get source name with row.Scan() %w", err)
+	}
+
+	return name, nil
+}
